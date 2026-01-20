0a6b8980b32f29a6d19df0b9a31090eca5dcdc3a
Aakash Loyar <aakashloyar@gmail.com>
2026-01-20 12:55:12 +0530

    refactor(track): update artist struct

--- track/internal/domain/artist.go
+++ track/internal/domain/artist.go
@@ -1,12 +1,13 @@
 package domain
+
 import "time"
+
 type Artist struct {
 	ID                 string
 	Name               string 
 	Bio                *string
 	Profile_image_url  *string
 	Create_At          time.Time
-
 }
 
 
