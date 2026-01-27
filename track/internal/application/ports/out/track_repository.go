130e8f8fc4ba12d0532238697c5e24dc1d567497
Aakash Loyar <aakashloyar@gmail.com>
2026-01-27 11:14:45 +0530

    feat(track): add ListTracks to TrackRepository port

--- track/internal/application/ports/out/track_repository.go
+++ track/internal/application/ports/out/track_repository.go
@@ -1,9 +1,12 @@
 package out
-import "github.com/aakashloyar/beats/track/internal/domain"
+import (
+	"github.com/aakashloyar/beats/track/internal/domain"
+	"github.com/aakashloyar/beats/track/internal/application/ports/in"
+)
 
 type TrackRepository interface {
-	Save(track *domain.Track) error
-	FindById(id string) (*domain.Track, error)
-	FindByArtist(artistID string) ([]*domain.Track, error)
+	Save(track domain.Track) error
+	FindById(id string) (domain.Track, error)
+	ListTracks(input in.ListTracksInput) ([]domain.Track, error)
 }
 
