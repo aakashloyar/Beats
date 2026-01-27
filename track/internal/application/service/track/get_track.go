1c36c5ac191dea8624575374c78650f002263c59
Aakash Loyar <aakashloyar@gmail.com>
2026-01-27 22:21:14 +0530

    refractor(track): gettrackservice input from string to struct

--- track/internal/application/service/track/get_track.go
+++ track/internal/application/service/track/get_track.go
@@ -1,14 +1,13 @@
-package track
+package service
 
 import (
-	"github.com/aakashloyar/beats/track/internal/application/ports/out"
+	"context"
 	"github.com/aakashloyar/beats/track/internal/application/ports/in/track"
-    "github.com/aakashloyar/beats/track/internal/domain"
-    "context"
+	"github.com/aakashloyar/beats/track/internal/application/ports/out"
 )
 
 type GetTrackService struct {
-	trackRepo    out.TrackRepository
+	trackRepo out.TrackRepository
 }
 
 func NewGetTrackService(trackRepo out.TrackRepository) in.GetTrackService {
@@ -16,11 +15,20 @@ func NewGetTrackService(trackRepo out.TrackRepository) in.GetTrackService {
 		trackRepo: trackRepo,
 	}
 }
-func (s *GetTrackService) Execute(ctx context.Context, trackID string) (domain.Track, error) {
-	x,err:=s.trackRepo.FindByID(trackID)
-	if err!=nil {
-		return domain.Track{},err
+func (s *GetTrackService) Execute(ctx context.Context, input in.GetTrackInput) (in.GetTrackOutput, error) {
+	x, err := s.trackRepo.FindByID(input.TrackID)
+	if err != nil {
+		return in.GetTrackOutput{}, err
 	}
-	return x,nil
+	track := in.GetTrackOutput{
+		ID:            x.ID,
+		ArtistID:      x.ArtistID,
+		AlbumID:       x.AlbumID,
+		CoverImageURL: x.CoverImageURL,
+		DurationMS:    x.DurationMS,
+		Language:      x.Language,
+		ReleaseDate:   x.ReleaseDate,
+		CreatedAt:     x.CreatedAt,
+	}
+	return track, nil
 }
-
