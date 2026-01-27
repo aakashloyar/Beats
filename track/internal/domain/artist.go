c917a9fed740b476763cedd7b467ebd2c0686690
Aakash Loyar <aakashloyar@gmail.com>
2026-01-27 21:16:41 +0530

    chore: gofmt

--- track/internal/domain/artist.go
+++ track/internal/domain/artist.go
@@ -3,11 +3,9 @@ package domain
 import "time"
 
 type Artist struct {
-	ID                 string
-	Name               string 
-	Bio                *string
-	ProfileImageURL  *string
-	CreatedAt          time.Time
+	ID              string
+	Name            string
+	Bio             *string
+	ProfileImageURL *string
+	CreatedAt       time.Time
 }
-
-
