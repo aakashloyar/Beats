a666981334e12e8b5ed8f9b665697a1180161cc8
Aakash Loyar <aakashloyar@gmail.com>
2026-01-27 18:43:22 +0530

    refactor(domain): rename Profile_image_url to ProfileImageURL

--- track/internal/domain/artist.go
+++ track/internal/domain/artist.go
@@ -6,8 +6,8 @@ type Artist struct {
 	ID                 string
 	Name               string 
 	Bio                *string
-	Profile_image_url  *string
-	Create_At          time.Time
+	ProfileImageURL  *string
+	CreatedAt          time.Time
 }
 
 
